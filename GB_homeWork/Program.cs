f9cbf7ce2c395ed5c0dd3a5f23eb4ed68c5cf69e
Anvar Khalitov <khalitoff@live.ru>
2021-01-17 12:26:51 +0500

    Добавлен проект-шаблон для ДЗ

---
+++ GB_homeWork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GB_homeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
