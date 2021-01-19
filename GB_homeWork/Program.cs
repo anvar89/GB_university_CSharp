fd545d32990b1cd3d58095f4f09d3fc6e7e33489
Anvar Khalitov <khalitoff@live.ru>
2021-01-19 21:48:44 +0500

    Задача 1 и 2 -решены

--- GB_homeWork/Program.cs
+++ GB_homeWork/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 
+
+//  Задача 1
+//  Запросить у пользователя минимальную и максимальную температуру за сутки и вывести среднесуточную температуру.
 namespace GB_homeWork
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double tempMin, tempMax, tempAver;
+
+            while (true)
+            {
+                Console.Write("Введите минимальую температуру: ");
+
+                if (double.TryParse(Console.ReadLine(), out tempMin))
+                    break;
+                else
+                    Console.WriteLine("Вы должны ввести число!");
+            }
+
+
+            while (true)
+            {
+                Console.Write("Введите максимальную температуру температуру: ");
+
+                if (double.TryParse(Console.ReadLine(), out tempMax))
+                {
+                    if (tempMax > tempMin) break;
+                    else
+                        Console.WriteLine("Максимальная температура не может быть меньше минимальной");
+                }
+                else
+                    Console.WriteLine("Вы должны ввести число!");
+            }
+
+            Console.WriteLine($"Среднесуточная температура: {(tempMin + tempMax) / 2}");
+
         }
     }
 }
---
+++ Task2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+//  Задача 2
+//  Запросить у пользователя порядковый номер текущего месяца и вывести его название.
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int monthNumber;
+            while (true)
+            {
+                Console.Write("Введите номер месяца: ");
+
+                if (int.TryParse(Console.ReadLine(), out monthNumber))
+                {
+                    if ((monthNumber > 1) && (monthNumber <= 12)) break;
+                    else
+                        Console.WriteLine("Введите число в диапазоне  1..12");
+                }
+                else
+                    Console.WriteLine("Вы должны ввести число!");
+            }
+
+            switch (monthNumber)
+            {
+                case 1: Console.WriteLine("Январь"); break;
+                case 2: Console.WriteLine("Февраль"); break;
+                case 3: Console.WriteLine("Март"); break;
+                case 4: Console.WriteLine("Апрель"); break;
+                case 5: Console.WriteLine("Май"); break;
+                case 6: Console.WriteLine("Июнь"); break;
+                case 7: Console.WriteLine("Июль"); break;
+                case 8: Console.WriteLine("Август"); break;
+                case 9: Console.WriteLine("Сентябрь"); break;
+                case 10: Console.WriteLine("Октябрь"); break;
+                case 11: Console.WriteLine("Ноябрь"); break;
+                case 12: Console.WriteLine("Декабрь"); break;
+            }
+
+            Console.ReadKey();
+
+        }
+        
+
+
+    }
+}
