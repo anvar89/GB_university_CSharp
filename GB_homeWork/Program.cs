1919b893c2b47274af7751bf2b2b1b7943861aeb
anvar89 <khalitoff@live.ru>
2021-01-24 13:05:05 +0500

    Задача 1 решена

--- GB_homeWork/Program.cs
+++ GB_homeWork/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 
-namespace GB_homeWork
+namespace Task1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Задача 1. Написать программу, выводящую элементы двухмерного массива по диагонали.");
+            int dimension = AskUserForInt("Введите размер массива: ");
+
+            //Заполнение и вывод массива
+            int[,] array = new int[dimension, dimension];
+            Random rnd = new Random();
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = rnd.Next(0, 100);
+
+                    Console.Write($"{array[i, j]}\t");
+
+                    if (j == array.GetLength(1) - 1) Console.WriteLine();
+                }
+            }
+
+            Console.Write("Главная диагональ: ");
+            for (int i = 0; i < dimension; i++)
+            {
+                Console.Write($"{array[i, i]} ");
+            }
+            
+            Console.Write("\nПобочная диагональ: ");
+            for (int i = 0; i < dimension; i++)
+            {
+                Console.Write($"{array[i, dimension - 1 - i]} ");
+            }
+        }
+
+        static int AskUserForInt(string info)
+        {
+            while (true)
+            {
+                Console.Write(info);
+                if(int.TryParse(Console.ReadLine(), out int result))
+                {
+                    if (result > 2) return result;
+                }
+                Console.WriteLine("Введите число больше 2");
+            }
         }
     }
 }
