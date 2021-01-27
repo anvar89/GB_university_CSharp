bad6d3bfc46ff8bd40b6e0dceee73526ae7727d2
anvar89 <36979151+anvar89@users.noreply.github.com>
2021-01-27 12:49:07 +0500

    Задача 1 решена, задача 2  - начато решение

--- GB_homeWork/Program.cs
+++ GB_homeWork/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 
-namespace GB_homeWork
+namespace Task1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Написать метод GetFullName(string firstName, string lastName, string patronymic), принимающий на вход ФИО в разных аргументах и возвращающий" +
+                " объединённую строку с ФИО. Используя метод, написать программу, выводящую в консоль 3–4 разных ФИО.");
+
+            Console.WriteLine(GetFullName("Владимир", "Ленин", "Ильич"));
+            Console.WriteLine(GetFullName("Иосиф", "Сталин", "Виссарионович"));
+            Console.WriteLine(GetFullName("Никита", "Хрущёв", "Сергеевич"));
+
+            Console.ReadKey();
         }
+
+        static string GetFullName(string firstName, string lastName, string patronymic) => $"{firstName} {patronymic} {lastName}";
     }
 }
---
+++ Task_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задача 2. Написать программу, принимающую на вход строку — набор чисел, разделенных пробелом, и возвращающую число — сумму всех чисел в строке." +
+                "Ввести данные с клавиатуры и вывести результат на экран.");
+
+            Console.WriteLine("Введите последовательность чисел:");
+            string[] userInput = Console.ReadLine().Split(" ");
+
+        }
+
+        public static int GetSum(params int[] arr)
+        {
+            int result = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                result += arr[i];
+            }
+            return result;
+        }
+
+
+
+        public static T AskUser<T>(string info, string errorMessage, Predicate<T> condition)
+        {
+            if (typeof(T) == typeof(string))
+            {
+                Console.
+                if (condition(string)) return 
+            }
+
+            if (typeof(T) == typeof(int))
+            {
+
+            }
+
+            throw new Exception($"Запрос у пользователя типа {T.ToString} ещё не реализован");
+        }
+    }
+}
