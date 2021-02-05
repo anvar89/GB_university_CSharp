073aa77087eaf48f3983565afe1e762627e03823
anvar89 <khalitoff@live.ru>
2021-02-05 07:19:55 +0500

    Задача 4 - решена на половину
    Нужно реализовать метод построения древа каталогов без рекурсии

---
+++ Task4/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задача 4. Сохранить дерево каталогов и файлов по заданному пути в текстовый файл — с рекурсией и без.");
+
+            string path = @"C:\Users\khali\Documents\gitHub\GB_university_CSharp";
+
+            filesTreeRecursive(new DirectoryInfo(path));
+        }
+
+        static void filesTreeLoop(DriveInfo driveInfo)
+        {
+
+        }
+
+        static void filesTreeRecursive(DirectoryInfo dirInfo, string ident)
+        {
+            FileInfo[] files = null;
+            DirectoryInfo[] subDirs = null;
+
+            string identMiddle = "├─";
+            string identLast = "└─";
+            string identLine = "│ ";
+            string identEmpty = "  ";
+
+
+            Console.WriteLine(dirInfo.Name);
+            try
+            {
+                files = dirInfo.GetFiles("*.*");
+                subDirs = dirInfo.GetDirectories();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            foreach (DirectoryInfo item in subDirs)
+            {
+                Console.Write(ident);
+                if ((item == subDirs[subDirs.Length - 1]) & (files.Length == 0))
+                {
+                    Console.Write(identLast);
+                    filesTreeRecursive(item, ident + identEmpty);
+                }
+                else
+                {
+                    Console.Write(identMiddle);
+                    filesTreeRecursive(item, ident + identLine);
+                }
+
+            }
+
+            foreach (FileInfo item in files)
+            {
+                Console.Write(ident);
+
+                if (item == files[files.Length - 1])
+                    Console.Write(identLast);
+                else
+                    Console.Write(identMiddle);
+
+                Console.WriteLine(item.Name);
+            }
+        }
+
+        static void filesTreeRecursive(DirectoryInfo dirInfo)
+        {
+            filesTreeRecursive(dirInfo, "");
+        }
+    }
+}
