f3b4a139aca68a4e1cb0558d6eedf47d4cb61ecc
anvar89 <khalitoff@live.ru>
2021-02-13 17:33:25 +0500

    Решено

--- GB_homeWork/Program.cs
+++ GB_homeWork/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 
 namespace GB_homeWork
 {
@@ -6,7 +7,89 @@ namespace GB_homeWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+       
+            Console.WriteLine("Сохранёные настройки:");
+            ReadAllSettings();
+            Console.WriteLine();
+
+            Greeting();
+
+            Console.WriteLine("Как вас зовут?");
+            WriteToAppProperty("UserName", Console.ReadLine());
+
+            Console.WriteLine("Какая у вас профессия?");
+            WriteToAppProperty("Job", Console.ReadLine());
+
+            Console.WriteLine("Сколько вам лет?");
+            WriteToAppProperty("Age", Console.ReadLine());
+
+            Console.ReadKey();
+        }
+        static void ReadAllSettings()
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+
+                if (appSettings.Count == 0)
+                {
+                    Console.WriteLine("AppSettings is empty.");
+                }
+                else
+                {
+                    foreach (var key in appSettings.AllKeys)
+                    {
+                        Console.WriteLine("{0} : {1}", key, appSettings[key]);
+                    }
+                }
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Ошибка при чтении свойств");
+            }
+        }
+        static void Greeting()
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+
+                if (appSettings.Count == 0)
+                {
+                    Console.WriteLine("Настройки приложения не найдены");
+                }
+                else
+                {
+                    Console.WriteLine(appSettings["Greeting"]);
+                }
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Ошибка при чтении настроек приложения");
+            }
+        }
+
+        static void WriteToAppProperty(string propertyName, string newValue)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[propertyName] == null)
+                {
+                    settings.Add(propertyName, newValue);
+                }
+                else
+                {
+                    settings[propertyName].Value = newValue;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Ошибка записи настроек");
+            }
         }
     }
 }
